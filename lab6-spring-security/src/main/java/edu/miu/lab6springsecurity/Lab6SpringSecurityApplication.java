a7c3c0f66806e5a63095d5b8ddf7a9b72d77929c
amgalanbe <amgalanbe@gmail.com>
2022-07-27 15:51:23 -0500

    git initialized lab6-spring-security

---
+++ lab6-spring-security/src/main/java/edu/miu/lab6springsecurity/Lab6SpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package edu.miu.lab6springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab6SpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab6SpringSecurityApplication.class, args);
+	}
+
+}
---
+++ lab6-spring-security/src/test/java/edu/miu/lab6springsecurity/Lab6SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.miu.lab6springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab6SpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
