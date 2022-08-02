71db55d4d2ad55a1acf4055c6fc9ca32b392cad6
bekmax <bekmax93@gmail.com>
2022-08-01 22:41:40 -0500

    aop implemented & corrected

--- spring-security/src/main/java/com/springsecurity/aspect/WaaOffensiveWords.java
+++ spring-security/src/main/java/com/springsecurity/aspect/WaaOffensiveWords.java
@@ -5,6 +5,8 @@ import com.springsecurity.domain.OffensiveWord;
 import com.springsecurity.domain.Product;
 import com.springsecurity.repository.AppUserRepository;
 import com.springsecurity.repository.OffensiveWordRepository;
+import com.springsecurity.security.userdetails.MyUserDetails;
+import com.springsecurity.security.userdetails.MyUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
@@ -12,10 +14,13 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.Field;
 import java.util.Date;
 import java.util.List;
 
@@ -31,30 +36,47 @@ public class WaaOffensiveWords {
 
     private final AppUserRepository appUserRepository;
     private final OffensiveWordRepository offensiveWordRepository;
+    private final MyUserDetailsService myUserDetailsService;
 
     @Pointcut("@annotation(com.springsecurity.aspect.OffensiveWordFilter)")
     public void run() {
     }
 
-    @Around("run() && args(.., @RequestBody body)")
-    public Object checkOffensiveWords(ProceedingJoinPoint proceedingJoinPoint, Object body) throws Throwable {
-        String username = SecurityContextHolder.getContext().getAuthentication().getName();
-        AppUser appUser = appUserRepository.findByUsername(username).get();
-        Product product = (Product) body;
-        List<OffensiveWord> words = offensiveWordRepository
-                .findByAppUserAndCreatedAtAfter(appUser, new Date(System.currentTimeMillis() - ALLOWED_TIME_LIMIT));
-        if (words.size() >= 5) {
+    @Around("run()")
+    public Object checkOffensiveWords(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        AppUser appUser = appUserRepository.findByUsername(myUserDetailsService.getCurrentUser().getUsername()).get();
+        int count = offensiveWordRepository.findByAppUserAndCreatedAtAfter(appUser, new Date(System.currentTimeMillis() - ALLOWED_TIME_LIMIT)).size();
+
+        if (count > 5) {
             httpServletResponse.setStatus(400);
             httpServletResponse.getWriter().write("Requests Limit has been reached. You need wait for " + ALLOWED_TIME_LIMIT / 60000 + " minutes.");
             return null;
-        } else {
-            if (product.getName().contains(OFFENSIVE_WORD)) {
-                offensiveWordRepository.save(new OffensiveWord(null, product.getName().replace(OFFENSIVE_WORD, "******"), new Date(), appUser));
-                httpServletResponse.setStatus(400);
-                httpServletResponse.getWriter().write("Offensive word!");
-                return null;
+        }
+
+
+        boolean offensiveDetected = changeOffensive(proceedingJoinPoint.getArgs());
+        if (offensiveDetected) {
+            offensiveWordRepository.save(new OffensiveWord(null, OFFENSIVE_WORD, new Date(), appUser));
+        }
+
+        return proceedingJoinPoint.proceed();
+    }
+
+    private boolean changeOffensive(Object... objects) throws IllegalAccessException {
+        boolean offensiveDetected = false;
+        for (Object o : objects) {
+            for (Field field : o.getClass().getDeclaredFields()) {
+                field.setAccessible(true);
+                if (field.getType().getSimpleName().equals(String.class.getSimpleName())) {
+                    String val = (String) field.get(o);
+                    if (val.toLowerCase().contains("spring")) {
+                        val = val.toLowerCase().replace("spring", "***ing");
+                        field.set(o, val);
+                        offensiveDetected = true;
+                    }
+                }
             }
         }
-        return proceedingJoinPoint.proceed();
+        return offensiveDetected;
     }
 }
--- spring-security/src/main/java/com/springsecurity/security/userdetails/MyUserDetailsService.java
+++ spring-security/src/main/java/com/springsecurity/security/userdetails/MyUserDetailsService.java
@@ -3,6 +3,7 @@ package com.springsecurity.security.userdetails;
 import com.springsecurity.domain.AppUser;
 import com.springsecurity.repository.AppUserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -17,6 +18,13 @@ import java.util.Optional;
 public class MyUserDetailsService implements UserDetailsService {
     private final AppUserRepository appUserRepository;
 
+    public UserDetails getCurrentUser() throws UsernameNotFoundException{
+        var auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth == null){
+            throw new UsernameNotFoundException("Login required");
+        }
+        return loadUserByUsername(auth.getName());
+    }
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Optional<AppUser> appUser = appUserRepository.findByUsername(username);
